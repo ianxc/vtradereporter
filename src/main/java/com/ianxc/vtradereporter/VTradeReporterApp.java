bfdaa885046277887da7674f8a61f10718f65c46
Ian Chen <ianchen7777@gmail.com>
2025-02-20 19:09:33 +1100

    Initial commit

---
+++ src/main/java/com/ianxc/vtradereporter/VTradeReporterApp.java
@@ -0,0 +1,13 @@
+package com.ianxc.vtradereporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VTradeReporterApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VTradeReporterApp.class, args);
+    }
+
+}
---
+++ src/test/java/com/ianxc/vtradereporter/VTradeReporterAppTests.java
@@ -0,0 +1,13 @@
+package com.ianxc.vtradereporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VTradeReporterAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
