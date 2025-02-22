9f63795b591f8d19157656360994d56a92ece49b
Ian Chen <ianchen7777@gmail.com>
2025-02-22 20:55:10 +1100

    XmlConfig comment updates

--- src/main/java/com/ianxc/vtradereporter/config/XmlConfig.java
+++ src/main/java/com/ianxc/vtradereporter/config/XmlConfig.java
@@ -15,7 +15,6 @@ public class XmlConfig {
     public DocumentBuilderFactory documentBuilderFactory() throws ParserConfigurationException {
         // Calling factory.newDocumentBuilder() is thread-safe.
         // Therefore, inject a DocumentBuilderFactory as a singleton bean.
-        // This tradeoff was made due to a slow classpath scan incurred on each DocumentBuilderFactory creation.
         // https://web.archive.org/web/20121214130319/http://jaxp.java.net/docs/spec/html/#plugabililty-thread-safety
         final var factory = DocumentBuilderFactory.newDefaultInstance();
         factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
