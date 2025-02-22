ac32b91879f485dcd194856e0a765f9e19ebdc75
Ian Chen <ianchen7777@gmail.com>
2025-02-22 20:51:12 +1100

    Remove webrequest arg

--- src/main/java/com/ianxc/vtradereporter/controller/GlobalExceptionHandler.java
+++ src/main/java/com/ianxc/vtradereporter/controller/GlobalExceptionHandler.java
@@ -5,14 +5,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(UnknownTradeFilterException.class)
-    public ProblemDetail handleUnknownTradeFilterException(UnknownTradeFilterException e, WebRequest request) {
+    public ProblemDetail handleUnknownTradeFilterException(UnknownTradeFilterException e) {
         return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
