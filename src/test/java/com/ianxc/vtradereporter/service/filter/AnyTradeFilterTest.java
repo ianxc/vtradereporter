9482be35f2913d0f09da7a4c3bfed50825f56f03
Ian Chen <ianchen7777@gmail.com>
2025-02-23 00:01:14 +1100

    1 more AnyTradeFilter test

--- src/test/java/com/ianxc/vtradereporter/service/filter/AnyTradeFilterTest.java
+++ src/test/java/com/ianxc/vtradereporter/service/filter/AnyTradeFilterTest.java
@@ -1,7 +1,12 @@
 package com.ianxc.vtradereporter.service.filter;
 
+import com.ianxc.vtradereporter.model.api.Trade;
 import com.ianxc.vtradereporter.model.api.TradePredefinedFilterKind;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,5 +16,18 @@ class AnyTradeFilterTest {
     @Test
     void when_getKind_then_shouldReturnAny() {
         assertThat(filter.getKind()).isEqualTo(TradePredefinedFilterKind.ANY);
+    }
+
+    @Test
+    void when_applyAppFilter_then_shouldKeepAll() {
+        final var startingTrades = List.of(
+                new Trade(1L, "abc", "bac", new BigDecimal("1.23"), "AUD", Instant.EPOCH),
+                new Trade(2L, "cab", "bac", new BigDecimal("2.34"), "NZD", Instant.MAX));
+
+        final var filtered = startingTrades.stream()
+                .filter(filter.getAppFilter())
+                .toList();
+
+        assertThat(filtered).isEqualTo(startingTrades);
     }
 }
